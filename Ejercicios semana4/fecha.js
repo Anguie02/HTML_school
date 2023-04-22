06d795eb5b2ef47d944d4cc3362fb6037478a1bf
Anguie02 <1487186@senti.pe>
2023-04-22 10:46:23 -0500

    Fecha actual

--- Ejercicios semana4/fecha.js
+++ Ejercicios semana4/fecha.js
@@ -3,10 +3,9 @@ let fecha1 = new Date("21/04/2023");
 let fecha2 = new Date("14/04/2023");
 
 
-let diferencia = fecha2.getTime()- fecha1.getTime();
-
+let diferencia = fecha2.getTime()- fecha1.getTime();                                                                                 
 
 let diasDiferencia = diferencia /1000 /60 /60/24;
 console.log(diasDiferencia);
 
-alert('El número de días que faltan es: ', diasDiferencia)
+console.log('El número de días que faltan es: '+ diasDiferencia);
